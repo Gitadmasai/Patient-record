330ffecd6f9a8f2dccaf8cedb3bd703eba12bb6f
Rahul Kumar <97456833+Gitadmasai@users.noreply.github.com>
2022-08-26 18:31:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,84 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Deepak',
+    lastName: 'kumar',
+    age: '19',
+    gender: 'male',
+    medicin: 'Advair Diskus 23'
+  },
+  {
+    id: 2,
+    firstName: 'Geeta',
+    lastName: 'kumari',
+    age: '20',
+    gender: 'female',
+    medicin: 'Crestor 12'
+  },
+  {
+    id: 3,
+    firstName: 'punam',
+    lastName: 'Hegdal',
+    age: '22',
+    gender: 'female',
+    medicin: 'Advair Diskus 66'
+  },
+  {
+    id: 4,
+    firstName: 'Rohit',
+    lastName: 'kumar',
+    age: '18',
+    gender: 'male',
+    medicin: 'Crestor 11'
+  },
+  {
+    id: 5,
+    firstName: 'Sumit',
+    lastName: 'kumar',
+    age: '25',
+    gender: 'male',
+    medicin: 'Advair Diskus 20'
+  },
+  {
+    id: 6,
+    firstName: 'Nitu',
+    lastName: 'kumari',
+    age: '19',
+    gender: 'female',
+    medicin: 'Synthroid 6'
+  },
+  {
+    id: 7,
+    firstName: 'Amit',
+    lastName: 'kumar',
+    age: '28',
+    gender: 'male',
+    medicin: 'Crestor 2'
+  },
+  {
+    id: 8,
+    firstName: 'Roshni',
+    lastName: 'kumari',
+    age: '23',
+    gender: 'female',
+    medicin: 'Synthroid 4'
+  },
+  {
+    id: 9,
+    firstName: 'Arnav',
+    lastName: 'kumar',
+    age: '30',
+    gender: 'male',
+    medicin: 'Nexium 6'
+  },
+  {
+    id: 10,
+    firstName: 'manisha',
+    lastName: 'kumari',
+    age: '23',
+    gender: 'female',
+    medicin: 'Synthroid 12'
+  }
+];
+
+export { employeesData };
